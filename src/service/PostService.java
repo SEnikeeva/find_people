08be3a0f7ea5ac820a19198a266d356bc1ef0686
Sumbel <sumbel.enikeeva@gmail.com>
2019-11-11 02:13:24 +0300

    added method addGamer

--- src/service/PostService.java
+++ src/service/PostService.java
@@ -3,29 +3,48 @@ package service;
 import helper.DbConnection;
 import model.Post;
 import model.User;
+import repository.PostRepositoryJdbcImpl;
 import repository.UserRepositoryJdbcImpl;
 
 import java.io.IOException;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 
 public class PostService {
-    public List<User> getGamers(Post post) throws SQLException, IOException, ClassNotFoundException {
+    public HashSet<User> getGamers(Post post) throws SQLException, IOException, ClassNotFoundException {
         Connection connection = new DbConnection().getConnection();
-        List<User> gamers = new ArrayList<>();
+        HashSet<User> gamers = new HashSet<>();
         try {
             PreparedStatement statement = connection.prepareStatement( "SELECT *  FROM" +
                     " post_user WHERE post = ?");
             statement.setInt(1, post.getId());
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                gamers.add(new UserRepositoryJdbcImpl().findByID(resultSet.getInt("user")));
+                gamers.add(new UserRepositoryJdbcImpl().findByID(resultSet.getInt("user_id")));
             }
 
         } catch (SQLException e) {
             throw new IllegalArgumentException(e);
         }
         return gamers;
+    }
+
+    public void addGamer(int post, int gamer) throws SQLException, IOException, ClassNotFoundException {
+        Connection connection = new DbConnection().getConnection();
+        PreparedStatement st = null;
+        try {
+            st = connection.prepareStatement(
+                    "INSERT INTO post_user(post, user_id) VALUES (?, ?)");
+        } catch (SQLException e) {
+            throw new IllegalArgumentException(e);
+        }
+        int i = 1;
+        st.setInt(i++, post);
+        st.setInt(i, gamer);
+        st.executeUpdate();
+        st.close();
+        Post post1 = new PostRepositoryJdbcImpl().findByID(post);
+        post1.setRequiredPlayers(post1.getRequiredPlayers()- 1);
+
     }
 }
