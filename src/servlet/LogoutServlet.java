dd205f41c4b936ac9560438851d25925d8178e85
Sumbel <sumbel.enikeeva@gmail.com>
2019-11-10 22:32:55 +0300

    added logout servlet

---
+++ src/servlet/LogoutServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.setAttribute("current_user", null);
+        eraseCookie(req, resp);
+        resp.sendRedirect("/login");
+    }
+
+    private void eraseCookie(HttpServletRequest req, HttpServletResponse resp) {
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null)
+            for (Cookie cookie : cookies) {
+                cookie.setValue("");
+                cookie.setPath("/");
+                cookie.setMaxAge(0);
+                resp.addCookie(cookie);
+            }
+    }
+}
